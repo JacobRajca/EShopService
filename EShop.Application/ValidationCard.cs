6aa24fc69d704a784e6586ab48bcf3f00349215e
JacobRajca <jakubrca@gmail.com>
2025-04-06 10:32:36 +0200

    zadanie 3

--- EShop.Application/ValidationCard.cs
+++ EShop.Application/ValidationCard.cs
@@ -7,6 +7,10 @@ public class ValidationCard: IValidationCard
 {
     public Boolean ValidateCardNumber(string cardNumber)
     {
+        cardNumber = cardNumber.Replace(" ", "").Replace("-", "");
+        if (!cardNumber.All(char.IsDigit))
+            return false;
+
         if (cardNumber.Length < 13)
         {
             throw new CardNumberTooShortException();
@@ -16,10 +20,6 @@ public class ValidationCard: IValidationCard
         {
             throw new CardNumberTooLongException();
         }
-        
-        cardNumber = cardNumber.Replace(" ", "").Replace("-", "");
-        if (!cardNumber.All(char.IsDigit))
-            return false;
 
         int sum = 0;
         bool alternate = false;
@@ -49,12 +49,23 @@ public class ValidationCard: IValidationCard
 
         if (Regex.IsMatch(cardNumber, @"^4(\d{12}|\d{15}|\d{18})$"))
             return "Visa";
-
-        if (Regex.IsMatch(cardNumber, @"^(5[1-5]\d{14}|2(2[2-9][1-9]|2[3-9]\d{2}|[3-6]\d{3}|7([01]\d{2}|20\d))\d{10})$"))
+        else if (Regex.IsMatch(cardNumber, @"^(5[1-5]\d{14}|2(2[2-9][1-9]|2[3-9]\d{2}|[3-6]\d{3}|7([01]\d{2}|20\d))\d{10})$"))
             return "MasterCard";
 
         if (Regex.IsMatch(cardNumber, @"^3[47]\d{13}$"))
             return "American Express";
+
+        if (Regex.IsMatch(cardNumber, @"^(6011\d{12}|65\d{14}|64[4-9]\d{13}|622(1[2-9][6-9]|[2-8]\d{2}|9([01]\d|2[0-5]))\d{10})$"))
+            return "Discover";
+
+        if (Regex.IsMatch(cardNumber, @"^(352[89]|35[3-8]\d)\d{12}$"))
+            return "JCB";
+
+        if (Regex.IsMatch(cardNumber, @"^3(0[0-5]|[68]\d)\d{11}$"))
+            return "Diners Club";
+
+        if (Regex.IsMatch(cardNumber, @"^(50|5[6-9]|6\d)\d{10,17}$"))
+            return "Maestro";
 
         throw new CardNumberInvalidException();
     }
